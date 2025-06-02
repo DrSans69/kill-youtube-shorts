87b0af96870e9ecf334ded00dee9044255894aa6
Sans <145922388+DrSans69@users.noreply.github.com>
2025-06-02 15:27:59 +0200

    change the way redirect work, now it may work on different browsers

--- scripts/redirect-shorts.js
+++ scripts/redirect-shorts.js
@@ -8,22 +8,19 @@ function fromShortsToRegular(videoId) {
     return `https://www.youtube.com/watch?v=${videoId}`;
 }
 
-function handleNavigation(event) {
-    const url = event.destination.url;
+function redirectIfShort(url) {
     const match = isShorts(url);
     if (match) {
-        location = fromShortsToRegular(match[1]);
+        location.replace(fromShortsToRegular(match[1]));
     }
 }
 
 chrome.storage.sync.get("redirectShorts", (result) => {
     if (!result.redirectShorts) return;
 
-    const url = location.href;
-    const match = isShorts(url);
-    if (match) {
-        location.replace(fromShortsToRegular(match[1]));
-    }
+    redirectIfShort(location.href);
 
-    navigation.addEventListener("navigate", handleNavigation);
+    window.addEventListener("yt-navigate-finish", () => {
+        redirectIfShort(location.href);
+    });
 });
